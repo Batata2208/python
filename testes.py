354bcf7052b142961a46b166ebbc609b8a963247
João Pedro Ricardo <joaopricardo25@gmail.com>
2024-08-30 22:21:40 -0300

    Add files via upload

---
+++ testes.py
@@ -0,0 +1,39 @@
+import os
+catalogo = []
+
+os.system("cls")
+
+def AdicionarProduto():
+	print("Digite o produto para ser cadastrado")
+	ProdutoAdicionar = input(str())
+	catalogo.append(ProdutoAdicionar)
+	print("\nProduto adicionado com sucesso")
+
+def RemoverProduto():
+	print("Digite o produto a ser removido \n")
+	ProdutoRemover = input(str())
+	catalogo.remove(ProdutoRemover)
+
+	print("\nProduto removido com sucesso")
+
+def EditarProduto():
+	print("Digite o produto a ser editado\n")
+	ProdutoEditar = input(str())
+	for i in catalogo:
+		if catalogo[i] = ProdutoEditar:
+			catalogo[i] = ProdutoEditar
+	
+	print("\nProduto editado com sucesso")
+	
+def lista():
+	for i in catalogo:
+  		print(i)
+		  
+
+AdicionarProduto()
+
+lista()
+
+EditarProduto()
+
+lista()
